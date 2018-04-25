d4beb210c9b8a4139f2ead70fda401b4061a70a1
Jared M. Peterson <developer@peteyinc.com>
2018-04-25 13:04:20 -0600

    added quote component

--- src/components/About/Me/Me.js
+++ src/components/About/Me/Me.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Me.css";
 
+import Quote from "../../Quote/Quote";
+
 const me = props => (
   <article className="Me">
     <h4>jmp</h4>
@@ -13,6 +15,13 @@ const me = props => (
         been able to make a career out of it even if it took me some time to
         figure out this is what I wanted to do.
       </p>
+      <Quote author="Steve Jobs">
+        Your work is going to fill a large part of your life, and the only way
+        to be truly satisfied is to do what you believe is great work. And the
+        only way to do great work is to love what you do. If you haven't found
+        it yet, keep looking. Don't settle. As with all matters of the heart,
+        you'll know when you find it.
+      </Quote>
       <p>
         I like playing card and board games, running, playing softball on
         Thursday nights, playing basketball whenever possible, and traveling
