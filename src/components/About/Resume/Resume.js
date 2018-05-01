4552279c87f3946a21e75f51203ecac576e3d49a
Jared M. Peterson <developer@peteyinc.com>
2018-05-01 16:18:46 -0600

    add experience

--- src/components/About/Resume/Resume.js
+++ src/components/About/Resume/Resume.js
@@ -13,6 +13,32 @@ const resume = props => (
       client and company performance using TensorFlow, Scikit-Learn and other ML
       libraries.
     </Experience>
+    <Experience
+      title="Senior Marketing Manager"
+      company="InfinIT Consulting"
+      start="2014"
+      end="2016"
+    >
+      Redesigned website UI/UX increasing lead conversion 80%. Implemented
+      development best practices including git, live/staging/dev environments.
+      Directed complete company rebrand, designed new identity including logo,
+      website and print materials.
+    </Experience>
+    <Experience
+      title="Marketing Manager"
+      company="Fenton Companies"
+      start="2009"
+      end="2013"
+    >
+      Design and develop websites for portfolio companies using HTML, CSS,
+      JavaScript, and jQuery. Responsible for technology related projects for
+      each portfolio company’s strategic business and organizational objectives.
+      Define and initiate projects, manage costs, scheduling and performance of
+      project components, while working to ensure project success.
+    </Experience>
+    {/* <Experience title="" company="" start="" end="">
+        Empty
+      </Experience> */}
   </article>
 );
 
